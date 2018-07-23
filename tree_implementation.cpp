f31344697558629012e91a08e4a20735e22ce872
Abhishek Singh <abhishekfromup@gmail.com>
2018-07-23 21:37:04 +0530

    added a function to find total no of paths in which sum of all nodes till leaf is equal to a specific number

--- tree_implementation.cpp
+++ tree_implementation.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int path_count = 0, sum = 0, num = 16;
+
 struct Node
 {
 	int key;
@@ -14,6 +16,7 @@ struct Node
 struct Node* newNode(int);
 void insertNewNode(struct Node*, int);
 void inorder(struct Node*);
+void findPaths(struct Node*);
 
 struct Node* newNode(int key)
 {
@@ -64,27 +67,86 @@ void inorder(struct Node* root)
 	inorder(root->right);
 }
 
+void findPaths(struct Node* root)
+{
+	if (!root)
+		return;
+
+	sum = sum + root->key;
+
+	findPaths(root->left);
+
+	if (sum == num)
+	{
+		path_count++;
+		// sum = sum - root->key;
+	}
+	
+
+	findPaths(root->right);
+	
+	if (sum == num)
+	{
+		path_count++;
+		// sum = sum - root->key;
+	}
+	sum = sum - root->key;
+}
 
 int main()
 {
-	struct Node* root = newNode(10);
-    root->left = newNode(11);
-    root->left->left = newNode(7);
-    root->right = newNode(9);
-    root->right->left = newNode(15);
-    root->right->right = newNode(8);
+	struct Node* root = newNode(1);
+	insertNewNode(root, 2);
+	insertNewNode(root, 3);
+	insertNewNode(root, 4);
+	insertNewNode(root, 1);
+	insertNewNode(root, 4);
+	insertNewNode(root, 4);
+	insertNewNode(root, 9);
+	insertNewNode(root, 8);
+	insertNewNode(root, 9);
+	insertNewNode(root, 5);
+	insertNewNode(root, 8);
+	insertNewNode(root, 0);
+	insertNewNode(root, 8);
+	insertNewNode(root, 0);
+	insertNewNode(root, 0);
+	insertNewNode(root, 0);
+	insertNewNode(root, 1);
+
+
+
+    // root->left = newNode(2);
+    // root->left->right = newNode(1);
+    // root->left->right->left = newNode(5);
+    // root->left->right->left->left = newNode(7);
+    // root->left->left = newNode(4);
+    // root->left->left->right = newNode(19); //
+    // root->left->left->left = newNode(8);
+    // root->left->left->left->left = newNode(1);
+    // root->left->left->left->right = newNode(0);
+    // root->left->left->left->right->right = newNode(1);
+    // root->left->left->left->right->left = newNode(1);
+
+    // root->right = newNode(3);
+    // root->right->left = newNode(4);
+    // // root->right->left = newNode(5);
+    // root->right->right = newNode(9);
+    // root->right->right->right = newNode(9);
+
+    // root->right->right->left = newNode(3);
+    // root->right->left->left = newNode(8);
  
     cout << "Inorder traversal before insertion:";
     inorder(root);
  
-    int key = 12;
-    insertNewNode(root, key);
-    insertNewNode(root, 100);
-    insertNewNode(root, 80);
+    // int key = 12;
  
     cout << endl;
     cout << "Inorder traversal after insertion:";
     inorder(root);
+    findPaths(root);
+    cout << "\n\n\n\nTotal Paths: " << path_count/2 << "\n\n";
 
 	return 0;
 }
